0068b7034f42cc861a9ad292b9321b37f09eab06
Tarun Rawat <tarunrawat8787@gmail.com>
2019-09-30 01:37:11 +0530

    add req per second functionality

--- expressStats.js
+++ expressStats.js
@@ -1,7 +1,11 @@
 var moment=require('moment');
+var EventEmitter=require('events');
 
 class expressStats{
-
+constructor(){
+   this.emitter=new EventEmitter();
+    
+}
 
     stats(req,res,next){
         let requestCount=0;
@@ -11,6 +15,9 @@ class expressStats{
         let finishedReq=0;
         let totalTime=0;
         let max_responseTime=0;
+        let highestReqPerSec=0;
+        let statusCodeMap=new Map();
+        let serverStartTime=moment();        
         return (req,res,next)=>{
             requestCount++;
             
@@ -19,22 +26,36 @@ class expressStats{
                "startTime":moment() 
             };
 
-            requestResMap.set(res,wrapper);
+            requstResMap.set(res,wrapper);
 
             res.on('finish',()=>{
                 finishedReq++;
-                let correspondingWrapperObj=requestResMap.get(res);
+                let correspondingWrapperObj=requstResMap.get(res);
                 let curr_time=moment();
                 let responseTime=curr_time.diff(correspondingWrapperObj.startTime,'milliseconds');
+                let curr_reqts=requestCount-finishedReq;
+                let time_diff=curr_time.diff(serverStartTime,"seconds");
+                let reqperSec=finishedReq/time_diff;
+                if(!isFinite(reqperSec))reqperSec=0; 
+                if(isFinite(reqperSec)&&highestReqPerSec<reqperSec)highestReqPerSec=reqperSec;
+
                 if(max_responseTime<=responseTime)max_responseTime=responseTime;
                 console.log('current request Time',responseTime);
-                console.log('current request Count',responseTime);
+                console.log('current request Count',finishedReq);
                 console.log('max request Time',max_responseTime);
-               
+                console.log('reqpersec ',reqperSec);
+                console.log('highest req per sec',highestReqPerSec);
+                console.log(statusCodeMap);
+                if(statusCodeMap.has(res.statusCode)==true){
+                    statusCodeMap.set(res.statusCode,statusCodeMap.get(res.statusCode)+1);
+                }else{
+                    statusCodeMap.set(res.statusCode,1);
+                }
+                console.log('**************************************');
 
             });
 
-
+                next();
         };
 
 
@@ -42,3 +63,5 @@ class expressStats{
     }
     
 }
+
+module.exports=new expressStats();
